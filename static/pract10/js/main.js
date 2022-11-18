dbeb51c6cf9a9316002b18459dec1dda35bf7091
elegantcookie <68335351+elegantcookie@users.noreply.github.com>
2022-11-18 05:47:26 -0500

    Add files via upload

---
+++ static/pract10/js/main.js
@@ -0,0 +1,13 @@
+import * as res from './resolve_elems.js'
+
+import * as cookies from './cookies.js'
+import * as likes from './likes.js'
+import * as signin from './signin.js'
+
+
+
+
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
